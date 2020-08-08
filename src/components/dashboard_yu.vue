de82bd79c6be22177abb2eb8bc5906c219afd775
yujianxia <yujian.xia@foxmail.com>
2020-08-09 00:41:22 +0800

    提交效果

--- src/components/dashboard_yu.vue
+++ src/components/dashboard_yu.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div class="dashboard_page">
     <!-- 头部 -->
@@ -17,15 +16,14 @@
           <p>
             <span> <span style="color: #677b94;">成航：</span>567474756</span>
             <span> <span style="color: #677b94;">藏航：</span>654757548</span>
-          </p> 
+          </p>
           <p>
-            <span> <span style="color: #677b94;">川航：</span>34737357373</span>  
+            <span> <span style="color: #677b94;">川航：</span>34737357373</span>
             <span> <span style="color: #677b94;">其他：</span>78047673</span>
-          </p> 
+          </p>
           <!-- 各航司每日总量 -->
           <div class="fs14 view_main_title">各航司每日总量</div>
           <div id="myCharts" class="daily_total_chart" ref="myCharts"></div>
-
         </div>
         <div class="view_main_top_m">
           <!-- 总报文量 -->
@@ -33,32 +31,24 @@
             总报文量：
           </div>
           <div class="fs14 view_main_title">各航司实时总量</div>
-
         </div>
         <div class="view_main_top_r">
           <div class="fs14 view_main_title">川航</div>
           <div class="donut_chart_box">
             <div>
-              <el-progress type="circle" :width="80" :percentage="25"></el-progress>
+              <div class="doug_chart_style" ref="doug_chart"></div>
             </div>
-            <div> <CircleComp :percentNum='12'></CircleComp> </div>
+            <div>
+              <div class="doug_chart_style" ref="doug_chart1"></div>
+            </div>
             <div>345</div>
           </div>
           <div class="fs14 view_main_title">藏航</div>
           <div class="donut_chart_box">
-
             <div>123</div>
             <div>234</div>
             <div>345</div>
           </div>
-          <div class="fs14 view_main_title">成航</div>
-          <div class="donut_chart_box">
-
-            <div>123</div>
-            <div>234</div>
-            <div>345</div>
-          </div>
-
         </div>
       </div>
       <!-- 下半部分 -->
@@ -66,29 +56,29 @@
         <!-- 第一个缓存区 -->
         <div style="padding-right: 10px;">
           <div class="fs14 view_main_title">tv缓存点</div>
-          <div  class="cache_chart" ref="myCharts111"></div>
+          <div class="cache_chart" ref="myCharts111"></div>
         </div>
         <!-- 第二个缓存区 -->
         <div style="padding-right: 10px;">
           <div class="fs14 view_main_title">ev缓存点</div>
-          <div  class="cache_chart" ref="myCharts222"></div>
+          <div class="cache_chart" ref="myCharts222"></div>
         </div>
         <!-- 第三个缓存区 -->
-        <div >
+        <div>
           <div class="fs14 view_main_title">tu缓存点</div>
-          <div  class="cache_chart" ref="myCharts333"></div>
+          <div class="cache_chart" ref="myCharts333"></div>
         </div>
         <!-- 第四个缓存区 -->
         <div style="padding-left: 10px;">
           <div class="fs14 view_main_title">redis和mq缓存点</div>
-          <div  class="cache_chart" ref="myCharts444"></div>
+          <div class="cache_chart" ref="myCharts444"></div>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
-import CircleComp from "./circle.vue"
+import CircleComp from "./circle.vue";
 export default {
   name: "dashboard_page",
   components: {
@@ -96,160 +86,287 @@ export default {
   },
   data() {
     return {
-      nowTime:"" //当前时间
+      nowTime: "" //当前时间
     };
   },
-  created () {
+  created() {},
+  mounted() {
+    // 图标
+    const doug_chart = this.$echarts.init(this.$refs.doug_chart);
+    let option_doug_chart = {
+      title: {
+        text: "你好",
+        left: "center",
+        top: "2%",
+        textStyle: {
+          color: "#00ce7d",
+          fontSize: 12,
+          align: "center",
+          fontWeight:"400"
+        }
+      },
+      color: ["#00ce7d", "#213b58"], //环形颜色
+
+      series: [
+        {
+          name: "",
+          type: "pie",
+          radius: ["60%", "70%"],
+          avoidLabelOverlap: false,
+          // silent: true, 
+          // hoverAnimation: false,
+          hoverOffset : 3,
+          center:["50%","58%"],
+          label: {
+            normal: {
+              show: true,
+              position: "center",
+              textStyle: {
+                //设置文字样式
+                fontSize: "12",
+                color: "#00ce7d"
+              },
+              formatter: function(data) {
+                if (data.name === '直接访问') {
+                  return data.value +"\n"+ data.percent +"%";
+                }else{
+                  return ''
+                }
+              }
+            },
+          },
+          data: [
+            { value: 100, name: "直接访问"},
+            { value: 330, name: "总量" }
+          ]
+        }
+      ]
+    };
+    doug_chart.setOption(option_doug_chart);
+    doug_chart.dispatchAction({ type: 'highlight', dataIndex: 0 });
+    // 图标
+
+    // 图标
+    const doug_chart1 = this.$echarts.init(this.$refs.doug_chart1);
+    let option_doug_chart1 = {
+      title: {
+        text: "你好",
+        left: "center",
+        top: "2%",
+        textStyle: {
+          color: "#42b6f6",
+          fontSize: 12,
+          align: "center",
+          fontWeight:"400"
+        }
+      },
+      color: ["#42b6f6", "#213b58"], //环形颜色
+
+      series: [
+        {
+          name: "",
+          type: "pie",
+          radius: ["60%", "70%"],
+          avoidLabelOverlap: false,
+          // silent: true, 
+          // hoverAnimation: false,
+          hoverOffset : 3,
+          center:["50%","58%"],
+          label: {
+            normal: {
+              show: true,
+              position: "center",
+              textStyle: {
+                //设置文字样式
+                fontSize: "12",
+                color: "#42b6f6"
+              },
+              formatter: function(data) {
+                if (data.name === '直接访问') {
+                  return data.value +"\n"+ data.percent +"%";
+                }else{
+                  return ''
+                }
+              }
+            },
+          },
+          data: [
+            { value: 100, name: "直接访问"},
+            { value: 330, name: "总量" }
+          ]
+        }
+      ]
+    };
+    doug_chart1.setOption(option_doug_chart1);
+    doug_chart1.dispatchAction({ type: 'highlight', dataIndex: 0 });
+    // 图标
 
 
-  },
-  mounted () {
+
+
     // 第一个图表
     const myCharts = this.$echarts.init(this.$refs.myCharts);
     let options = {
-       color : ['green','#2f4554', '#61a0a8', '#d48265', '#91c7ae','#749f83', '#ca8622'],
-        tooltip: {
-            trigger: 'axis',
-            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+      color: [
+        "green",
+        "#2f4554",
+        "#61a0a8",
+        "#d48265",
+        "#91c7ae",
+        "#749f83",
+        "#ca8622"
+      ],
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          // 坐标轴指示器，坐标轴触发有效
+          type: "shadow" // 默认为直线，可选为：'line' | 'shadow'
+        }
+      },
+      grid: {
+        top: "10px",
+        left: "10px",
+        right: "14px",
+        bottom: "18px",
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: "category",
+          data: ["08-12", "08-13", "08-14", "08-15", "08-16", "08-17", "08-18"],
+          axisLabel: {
+            show: true,
+            textStyle: {
+              color: "#fff",
+              fontsize: "12"
+            },
+            interval: 0,
+            rotate: -30 //文字倾斜角度
+          }
+        }
+      ],
+      yAxis: [
+        {
+          type: "value",
+          splitLine: {
+            show: true,
+            lineStyle: {
+              color: ["#213853"],
+              width: 1,
+              type: "solid"
             }
+          },
+          axisLabel: {
+            show: true,
+            textStyle: {
+              color: "#fff"
+            },
+            formatter: function(value) {
+              return value / 1000 + "k";
+            }
+          }
+        }
+      ],
+      series: [
+        {
+          name: "直接访问",
+          type: "bar",
+          data: [3246460, 356432, 3351, 3354, 3940, 3234230, 323230]
         },
-        grid: {
-            top: '10px',
-            left: '10px',
-            right: '14px',
-            bottom: '18px',
-            containLabel: true
+        {
+          name: "邮件营销",
+          type: "bar",
+          stack: "广告",
+          data: [3246460, 356432, 3351, 3354, 3940, 3234230, 323230]
         },
-        xAxis: [
-            {
-                type: 'category',
-                data: ['08-12', '08-13', '08-14', '08-15', '08-16', '08-17', '08-18'],
-                axisLabel: {  
-                    show: true,
-                    textStyle: {
-                        color: '#fff',
-                        fontsize:"12"
-                    },
-                    interval:0,  
-                    rotate:-30  //文字倾斜角度
-                }
-            }
-        ],
-        yAxis: [
-            {
-                type: 'value',
-                splitLine: {
-                    show: true,
-                    lineStyle:{
-                      color: ['#213853'],
-                      width: 1,
-                      type: 'solid'
-                  }
-            　　},
-                axisLabel: {
-                    show: true,
-                    textStyle: {
-                        color: '#fff'
-                    },
-                    formatter: function(value) {
-                        return value / 1000 +'k';
-                    }
-                }
-            }
-        ],
-        series: [
-            {
-                name: '直接访问',
-                type: 'bar',
-                data: [3246460, 356432, 3351, 3354, 3940, 3234230, 323230]
-            },
-            {
-                name: '邮件营销',
-                type: 'bar',
-                stack: '广告',
-                data: [3246460, 356432, 3351, 3354, 3940, 3234230, 323230]
-            },
-            {
-                name: '联盟广告',
-                type: 'bar',
-                stack: '广告',
-                data: [3246460, 356432, 3351, 3354, 3940, 3234230, 323230]
-            },
-        ]
+        {
+          name: "联盟广告",
+          type: "bar",
+          stack: "广告",
+          data: [3246460, 356432, 3351, 3354, 3940, 3234230, 323230]
+        }
+      ]
     };
     myCharts.setOption(options);
-
 
     const myCharts111 = this.$echarts.init(this.$refs.myCharts111);
     const myCharts222 = this.$echarts.init(this.$refs.myCharts222);
     const myCharts333 = this.$echarts.init(this.$refs.myCharts333);
     const myCharts444 = this.$echarts.init(this.$refs.myCharts444);
     let option111 = {
-    tooltip: {
-        trigger: 'axis',
+      tooltip: {
+        trigger: "axis",
         axisPointer: {
-            type: 'shadow'
+          type: "shadow"
         }
-    },
-    grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
+      },
+      grid: {
+        left: "3%",
+        right: "4%",
+        bottom: "3%",
         containLabel: true
-    },
-    xAxis: {
-        type: 'value',
+      },
+      xAxis: {
+        type: "value",
         boundaryGap: [0, 0.01]
-    },
-    yAxis: {
-        type: 'category',
-        data: ['巴西', '印尼', '美国', '印度', '中国', '世界人口(万)']
-    },
-    series: [
+      },
+      yAxis: {
+        type: "category",
+        data: ["巴西", "印尼", "美国", "印度", "中国", "世界人口(万)"]
+      },
+      series: [
         {
-            name: '2011年',
-            type: 'bar',
-            data: [18203, 23489, 29034, 104970, 131744, 630230]
+          name: "2011年",
+          type: "bar",
+          data: [18203, 23489, 29034, 104970, 131744, 630230]
         },
         {
-            name: '2012年',
-            type: 'bar',
-            data: [19325, 23438, 31000, 121594, 134141, 681807]
+          name: "2012年",
+          type: "bar",
+          data: [19325, 23438, 31000, 121594, 134141, 681807]
         }
-    ]
-};
+      ]
+    };
 
     myCharts111.setOption(option111);
     myCharts222.setOption(option111);
     myCharts333.setOption(option111);
     myCharts444.setOption(option111);
   },
-  beforeDestroy(){
-
-  },
-  methods: {
-  
-
-  }
+  beforeDestroy() {},
+  methods: {}
 };
 </script>
 <style lang="scss">
-p{
+p {
   margin: 0;
   padding: 0;
 }
-.dashboard_page{
-  .pointer{cursor: pointer;}
-  .fs12{font-size: 12px;}
-  .fs14{font-size: 14px;}
-  .fs16{font-size: 16px;}
+.dashboard_page {
+  .pointer {
+    cursor: pointer;
+  }
+  .fs12 {
+    font-size: 12px;
+  }
+  .fs14 {
+    font-size: 14px;
+  }
+  .fs16 {
+    font-size: 16px;
+  }
   color: #fff;
   word-break: break-all;
   width: 100vw;
   height: 100vh;
   background-color: #17273b;
-  .dashboard_header{
+  // color: #00ce7d;
+  // color: #42b6f6;
+  // color: #ff9560;
+  // color: #e55541;
+  // color: #fff04b;
+  // color: #f95aff;
+  .dashboard_header {
     background-color: #1c344f;
     height: 50px;
     display: flex;
@@ -258,39 +375,39 @@ p{
     padding: 0 20px;
     box-sizing: border-box;
     margin-bottom: 10px;
-    .header_title{
+    .header_title {
       font-size: 20px;
       letter-spacing: 3px;
     }
   }
-  .view_main{
+  .view_main {
     // outline: 1px solid red;
     width: 100%;
     height: calc(100vh - 60px);
     padding: 0 10px;
     box-sizing: border-box;
-    .view_main_title{
+    .view_main_title {
       height: 36px;
       line-height: 36px;
       background-color: #1c344f;
       padding: 0 10px;
       box-sizing: border-box;
     }
-    .view_main_top{
+    .view_main_top {
       display: flex;
       height: 45%;
-      .view_main_top_l{
+      .view_main_top_l {
         padding-right: 10px;
         box-sizing: border-box;
         width: 25%;
-        &>p{
+        & > p {
           font-size: 12px;
           height: 30px;
           padding: 0 10px;
           box-sizing: border-box;
           background-color: #1c3049;
           display: flex;
-          &>span{
+          & > span {
             display: inline-block;
             line-height: 30px;
             width: 50%;
@@ -298,48 +415,51 @@ p{
             border-bottom: 1px solid #213853;
           }
         }
-        .daily_total_chart{
+        .daily_total_chart {
           width: 100%;
           height: calc(100% - 134px);
           background-color: #1c3049;
         }
       }
-      .view_main_top_m{
+      .view_main_top_m {
         // outline: 1px solid red;
         width: 50%;
-        .view_main_top_m_total{
-          height:50px;
+        .view_main_top_m_total {
+          height: 50px;
           text-align: center;
           background-color: #1c3049;
         }
       }
-      .view_main_top_r{
+      .view_main_top_r {
         // outline: 1px solid red;
         padding-left: 10px;
         box-sizing: border-box;
         width: 25%;
-        .donut_chart_box{
+        .donut_chart_box {
           width: 100%;
-          height: calc((100% - 108px) / 3);
+          height: calc((100% - 72px) / 2);
           background-color: #1c3049;
           display: flex;
-          &>div{
+          & > div {
             width: calc(100% / 3);
             height: 100%;
+            .doug_chart_style {
+              height: 100%;
+            }
           }
         }
       }
     }
-    .view_main_bottom{
+    .view_main_bottom {
       margin-top: 10px;
       display: flex;
-      height: calc(55% - 10px) ;
+      height: calc(55% - 10px);
       display: flex;
-      &>div{
+      & > div {
         width: 25%;
         box-sizing: border-box;
         height: 100%;
-        .cache_chart{
+        .cache_chart {
           width: 100%;
           height: calc(100% - 36px);
           background-color: #1c3049;
@@ -347,6 +467,5 @@ p{
       }
     }
   }
-
 }
 </style>
--- src/components/iii.vue
+++ src/components/iii.vue
@@ -1,101 +1,116 @@
 // 个人预存款调货
 <template>
   <div class="person-cargo-count-page">
-    <DataModel class="borderLeftNone" :child-data="transferData"></DataModel>
-    <!-- 查询 -->
-    <div class="search_container">
-      查询：
-      <el-input size="small" clearable placeholder="会员ID/姓名/电话" v-model="searchInfo" style="width:200px;margin-right:20px"></el-input>
-      <el-date-picker v-model="startMonth" value-format="yyyy-MM" style="width:150px;margin-right:20px" type="month" size="small" placeholder="选择开始月">
-      </el-date-picker>
-      <el-date-picker v-model="endMonth" value-format="yyyy-MM" style="width:150px;margin-right:20px" type="month" size="small" placeholder="选择结束月">
-      </el-date-picker>
-      <i @click="search_submit" class="el-icon-search"></i>
+    <div id="testTip"></div>
+    <div class="animationText loading">
+      3.1 文字扫光效果 -webkit-keyframes
     </div>
-    <el-table :data="tableData" style="width: 100%" :header-cell-style="{background:'#E4E7ED'}">
-          <el-table-column prop="date" label="调货编号" width="180" align="center">
-          </el-table-column>
-          <el-table-column prop="date" label="调货人姓名" align="center">
-          </el-table-column>
-          <el-table-column prop="date" label="电话" align="center">
-          </el-table-column>
-          <el-table-column prop="date" label="商品名称" align="center">
-          </el-table-column>
-          <el-table-column prop="date" label="调货数量" align="center">
-          </el-table-column>
-          <el-table-column prop="date" label="级别" align="center">
-          </el-table-column>
-          <el-table-column prop="date" label="预存款结余" align="center">
-          </el-table-column>
-          <el-table-column prop="date" label="调货人库存" align="center">
-          </el-table-column>
-          <el-table-column prop="date" label="收货人姓名" align="center">
-          </el-table-column>
-          <el-table-column prop="date" label="收货人电话" align="center">
-          </el-table-column>
-          <el-table-column prop="date" label="收货人库存" align="center">
-          </el-table-column>
-          <el-table-column prop="date" label="调货状态" align="center">
-          </el-table-column>
-          <el-table-column prop="date" label="调货日期" align="center">
-          </el-table-column>
-        </el-table>
-        <!-- 翻页 -->
-        <el-pagination style="text-align:right" background @current-change="handleCurrentChange" :current-page.sync="params.page" :page-size="params.size" layout="prev, pager, next" :total="totalCount">
-        </el-pagination>
   </div>
 </template>
 <script>
-import DataModel from "../common/dataModel";
 export default {
   name: "personCargo",
-  components: {
-    DataModel
-  },
   data() {
-    return {
-      transferData: [
-        {
-          title: "今日调货",
-          value: 111,
-          unit:"笔"
+    return {};
+  },
+  mounted() {
+    // 基于准备好的dom，初始化echarts实例
+    let myChart = this.$echarts.init(document.getElementById("testTip"));
+    // 绘制图表
+    myChart.setOption({
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          // 坐标轴指示器，坐标轴触发有效
+          type: "shadow" // 默认为直线，可选为：'line' | 'shadow'
         },
+        confine: true, //显示在图标区域内
+        formatter: function(params) {
+          let astr = "";
+          params.forEach(ele => {
+            astr += `
+              <div class="itemStyle">
+                <i style="background: ${ele.color};"></i>
+                <span>${ele.seriesName}：${ele.value}</span>
+              </div>
+            `;
+          });
+          const allItem = `<div class="tooltipStyle">
+            <p>${params[0].axisValueLabel}</p>
+            ${astr}
+          <div>`;
+          return allItem;
+        }
+      },
+      xAxis: [
         {
-          title: "本月调货",
-          value: 2222,
-          unit:"笔"
-        },
-      ],
-      searchInfo: "", //查询str
-      startMonth: "", //开始月
-      endMonth: "", //结束月
-      tableData: [
-        {
-          date: "2016-05-02"
+          type: "category",
+          data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"]
         }
       ],
-      totalCount: 30,
-      params: {
-        page: 1,
-        size: 10
-      }
-    };
+      yAxis: [
+        {
+          type: "value"
+        }
+      ],
+      series: [
+        {
+          name: "直接访问",
+          type: "bar",
+          data: [320, 332, 301, 334, 390, 330, 320]
+        },
+        {
+          name: "邮件营销",
+          type: "bar",
+          stack: "广告",
+          data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+          name: "联盟广告",
+          type: "bar",
+          stack: "广告",
+          data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+          name: "视频广告",
+          type: "bar",
+          stack: "广告",
+          data: [150, 232, 201, 154, 190, 330, 410]
+        },
+        {
+          name: "搜索引擎",
+          type: "bar",
+          data: [862, 1018, 964, 1026, 1679, 1600, 1570]
+        },
+        {
+          name: "百度",
+          type: "bar",
+          barWidth: 5,
+          stack: "搜索引擎",
+          data: [620, 732, 701, 734, 1090, 1130, 1120]
+        },
+        {
+          name: "谷歌",
+          type: "bar",
+          stack: "搜索引擎",
+          data: [120, 132, 101, 134, 290, 230, 220]
+        },
+        {
+          name: "必应",
+          type: "bar",
+          stack: "搜索引擎",
+          data: [60, 72, 71, 74, 190, 130, 110]
+        },
+        {
+          name: "其他",
+          type: "bar",
+          stack: "搜索引擎",
+          data: [62, 82, 91, 84, 109, 110, 120]
+        }
+      ]
+    });
   },
-  methods: {
-    // 提交函数
-    search_submit() {
-      console.log("searchInfo", this.searchInfo);
-      console.log("startMonth", this.startMonth);
-      console.log("endMonth", this.endMonth);
-    },
-    handleCurrentChange(val){
-      console.log('params',this.params);
-    },
-    //根据充值人电话查询信息
-    searchPerson(){
-      console.log('111111111111111111',);
-    }
-  }
+  methods: {}
 };
 </script>
 <style lang="scss">
@@ -103,36 +118,95 @@ export default {
   cursor: pointer;
   color: #3f51b5;
 }
-.person-cargo-count-page{
-  p {
-    margin: 0;
+.person-cargo-count-page {
+  width: 100vw;
+  height: 100vh;
+  background: gray;
+  #testTip {
+    width: 300px;
+    height: 300px;
   }
-  .search_container {
-    height: 40px;
-    background-color: #f8f8f8;
-    margin: 20px 0;
-    padding: 0 10px;
-    display: flex;
-    align-items: center;
-    .el-icon-search {
-      cursor: pointer;
-      font-size: 24px;
-    }
-  } 
-  .el-icon-search {
-    cursor: pointer;
-    font-size: 24px;
+}
+.tooltipStyle {
+  // height: 300px;
+  width: 426px;
+  padding: 10px;
+  border-radius: 8px;
+  box-sizing: border-box;
+  z-index: 9999999;
+  & > p {
+    margin: 0 0 4px;
+    padding: 0;
   }
-  .cargoBox{
-    display: flex;
-    justify-content: flex-start;
-    &>div{
-      width: 50%;
-      height:auto;
-    }
-    .border_r{
-      border-right: 1px solid #bbb;
+  .itemStyle {
+    min-height: 24px;
+    line-height: 24px;
+    width: 134px;
+    float: left;
+    color: #fff;
+    & > i {
+      width: 10px;
+      height: 10px;
+      display: inline-block;
+      border-radius: 50%;
     }
   }
+
+  // 文字效果
+}
+
+// @keyframes masked-nimation {
+//   0% {
+//     // background-position: 0 0;
+//     text-shadow: 0 0 10px, 0 0 4px;
+//   }
+//   20% {
+//     // background-position: -100% 0;
+//     text-shadow: 0 0 8px, 0 0 4px;
+//   }
+//   40% {
+//     // background-position: -100% 0;
+//     text-shadow: 0 0 6px, 0 0 4px;
+//   }
+//   60% {
+//     // background-position: -100% 0;
+//     text-shadow: 0 0 4px, 0 0 4px;
+//   }
+//   80% {
+//     // background-position: -100% 0;
+//     text-shadow: 0 0 6px, 0 0 4px;
+//   }
+//   100% {
+//     // background-position: -100% 0;
+//     text-shadow: 0 0 10px, 0 0 4px;
+//   }
+// }
+
+@keyframes masked-nimation {
+  0% {
+    background-position: 0 0;
+  }
+  50% {
+    background-position: -50% 0;
+  }
+  100% {
+    background-position: -100% 0;
+  }
+}
+.animationText {
+
+
+  width: 290px;
+  height: 30px;
+    margin: 0 auto;
+    margin-top: 100px;
+    font-size: 20px;
+    text-align: center;
+    background: -webkit-linear-gradient(left,#147B96,#1786c7 25%,#82cfe2 50%,#0594e6 75%,#147B96);
+    // color: rgb(77, 211, 73); 
+    color: transparent;
+    -webkit-background-clip: text;  
+    background-size: 200% 100%;
+    animation: 2s masked-nimation infinite linear;
 }
 </style>
